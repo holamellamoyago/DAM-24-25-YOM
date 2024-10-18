6cad89d0f1bc9e4e97e844de88e800217c73f632
Yago Otero <yagootero2003@gmail.com>
2024-10-18 13:55:28 +0200

    Implemendado factura.java

--- src/ud2/ejerciciosPropuestos/E0204.java
+++ src/ud2/ejerciciosPropuestos/E0204.java
@@ -1,7 +1,18 @@
 package ud2.ejerciciosPropuestos;
+import java.util.Scanner;
+
 
 public class E0204 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce un numero decimal");
+        double numero = sc.nextDouble();
+        sc.close();
+
+        if (numero < -0.5 || numero > 0.5) {
+            System.out.println("El numero esta mas cerca de -1 o 1");
+        } else{
+            System.out.println("El numero es casi-cero");
+        }
     }
 }
