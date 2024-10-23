6d049a324acf755aeb3438d658e5dd74c66f1add
Yago Otero <yagootero2003@gmail.com>
2024-10-23 14:22:16 +0200

    Añadidos dos ejercicios

---
+++ src/ud2/ejerciciosPropuestos/E0213.java
@@ -0,0 +1,51 @@
+package ud2.ejerciciosPropuestos;
+
+import java.util.Scanner;
+
+public class E0213 {
+    public static void main(String[] args) {
+        System.out.println("Introduce hora, minuto y segundos");
+        Scanner sc = new Scanner(System.in);
+        int hora = sc.nextInt();
+        int minuto = sc.nextInt();
+        int segundo = sc.nextInt();
+
+        // switch (segundo) {
+        // case 59 : minuto++;
+        // default:
+        // switch (minuto) {
+        // case 59:
+        // hora++;
+        // break;
+
+        // default:
+        // break;
+        // }
+        // break;
+        // }
+
+        if (segundo >= 0 && segundo < 60) {
+            if (segundo == 59) {
+                segundo = 0;
+                minuto++;
+                if (minuto == 60) {
+                    minuto = 0;
+                    hora++;
+                }
+            } else {
+                segundo++;
+            }
+
+            if (minuto >= 0 && minuto < 60) {
+                if (minuto == 59) {
+                    hora++;
+                }
+            }
+        } else {
+            System.out.println("Hora no valida");
+        }
+
+        System.out.println("Hora:" + hora + " Minutos: " + minuto + " segundos : " + segundo);
+    }
+
+}
