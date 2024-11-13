f20464d4961dd043b9bab22737d8f973e8d3f95c
Yago Otero <yagootero2003@gmail.com>
2024-11-13 14:30:15 +0100

    Ejercicios dia 13-11

---
+++ src/ud2/ejerciciosPropuestos/E0411_corregido.java
@@ -0,0 +1,31 @@
+package ud2.ejerciciosPropuestos;
+
+import java.util.Scanner;
+import ud2.Util;
+
+public class E0411_corregido {
+
+    public static int mcd(int a, int b) {
+        int mcd = 0;
+
+        if (a == 0) {
+            mcd = b;
+        } else if (b == 0) {
+            mcd = a;
+        } else if (a >= b) {
+            mcd = mcd(a - b, b);
+        } else {
+            mcd = mcd(a, b - a);
+        }
+
+        return mcd;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(mcd(4, 8));
+
+            
+    
+    }
+
+}
---
+++ src/ud2/ejerciciosPropuestos/recursividad/Potencia.java
@@ -0,0 +1,17 @@
+package ud2.ejerciciosPropuestos.recursividad;
+
+public class Potencia {
+    public static int pontencia(int base, int p){
+        int contador; 
+        if (contador ) {
+            
+        }
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(pontencia(4, 2));
+    }
+
+    
+}
