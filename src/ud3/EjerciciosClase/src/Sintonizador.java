b836517d2d0ea5a56f7f729d674aeb7f3db96b47
Yago Otero <yagootero2003@gmail.com>
2024-12-03 14:28:55 +0100

    Ejercicios día 03-12

---
+++ src/ud3/EjerciciosClase/src/Sintonizador.java
@@ -0,0 +1,45 @@
+public class Sintonizador {
+    private double frecuencia;
+    private double memoria1;
+    private double memoria2;
+    private double memoria3;
+
+    public Sintonizador(double frecuencia) {
+
+        if (frecuencia < 80 || frecuencia>108) {
+            this.frecuencia = 80;
+        } else {
+
+            this.frecuencia = frecuencia;
+        }
+
+    }
+
+
+    void mostrarFrecuencia() {
+        System.out.println(frecuencia + "Mhz");
+    }
+
+    void bajarFrecuencia() {
+        if (frecuencia <= 0) {
+            System.out.println("Ya alcanzaste el mínimo");
+            frecuencia = 80;
+        } else {
+            frecuencia = -0.5;
+        }
+
+        System.out.println("Ahora la frecuencia es: " + frecuencia);
+
+    }
+
+    void subirFrecuencia() {
+        if (frecuencia < 0) {
+            frecuencia = 0.5;
+        } else {
+            frecuencia = +0.5;
+        }
+
+        System.out.println("Ahora la frecuencia es: " + frecuencia);
+
+    }
+}
