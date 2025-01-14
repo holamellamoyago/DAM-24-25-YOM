296c7b88f3909954eea28c280523059a3a52dd6e
Yago Otero <yagootero2003@gmail.com>
2025-01-14 23:24:23 +0100

    Ejercicios 14 del 1 y más

--- src/ud4/Ejercicios/E0507.java
+++ src/ud4/Ejercicios/E0507.java
@@ -7,23 +7,34 @@ import ud4.ArraysUtil;
 public class E0507 {
     static Integer[] sinRepetidos(Integer t[]) {
         Integer p[] = new Integer[0];
-        
+        int contadorp = 0;
+
         Arrays.sort(t);
         System.out.println(Arrays.toString(t));
 
-        System.out.println(Arrays.binarySearch(t, 3));
-
+    
+        
+        
+        
         for (int i = 0; i < t.length; i++) {
-            if (Arrays.binarySearch(t, 3)) {
-                
+            int posicion = Arrays.binarySearch(p, t[i]);
+            if (posicion < 0 ) {
+                p =  Arrays.copyOf(p, p.length+1);
+                p[contadorp] = t[i];
+                Arrays.toString(p);
+                contadorp++;
             }
+
         }
 
         return p;
     }
 
     public static void main(String[] args) {
-        sinRepetidos(ArraysUtil.arrayAleatorio(10, 1, 6));
+        Integer t[] = ArraysUtil.arrayAleatorio(10, 1, 6);
 
+         t = sinRepetidos(t);
+
+        System.out.println(Arrays.toString(t));
     }
 }
