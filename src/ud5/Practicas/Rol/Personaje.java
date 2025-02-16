b502c98f4cb99d20411dac53211983400c8298b0
Yago Otero <yagootero2003@gmail.com>
2025-02-17 00:06:55 +0100

    Ejercicios 16 del 02

--- src/ud5/Practicas/Rol/Personaje.java
+++ src/ud5/Practicas/Rol/Personaje.java
@@ -1,27 +1,92 @@
 package ud5.Practicas.Rol;
 
+import java.util.Random;
+
 public class Personaje {
-    enum raza {
+    enum Raza {
         HUMANO, ELFO, ENANO, HOBBIT, ORCO, TROLL
     }
 
     String nombre;
+    Raza raza;
+    int experiencia, puntosDeVida;
+    byte nivel;
+    double fuerza, agilidad, constitucion;
 
-    int fuerza, agilidad, constitucion, nivel, experiencia, puntosDeVida;
 
-    public Personaje(String nombre, int fuerza, int agilidad, int constitucion, int nivel, int experiencia,
-            int puntosDeVida) {
+    int vidaInicial;
+
+    public Personaje(String nombre, int fuerza, int agilidad, int constitucion, byte nivel, int experiencia,
+            int puntosDeVida, Raza raza) {
         this.nombre = nombre;
         this.fuerza = validarValor(fuerza);
         this.agilidad = validarValor(agilidad);
         this.constitucion = validarValor(constitucion);
-        this.nivel = validarValor(nivel);
+        this.nivel = nivel < 0 ? 1 : nivel;
         this.experiencia = experiencia < 0 ? 0 : experiencia;
-        this.puntosDeVida = validarValor(puntosDeVida);
+        this.puntosDeVida = 50 + validarValor(constitucion);
+        this.raza = raza;
+
+        vidaInicial = puntosDeVida;
 
     }
 
+    public Personaje(String nombre, Raza raza, int fuerza, int agilidad, int constitucion) {
+        this.nombre = nombre;
+        this.raza = raza;
+        this.fuerza = validarValor(fuerza);
+        this.agilidad = validarValor(agilidad);
+        this.constitucion = validarValor(constitucion);
+        this.nivel = 1;
+        this.experiencia = 0;
+        this.puntosDeVida = 50 + validarValor(constitucion);
+
+        vidaInicial = puntosDeVida;
+
+        
+    }
+
+    public Personaje(String nombre, Raza raza){
+        this.nombre = nombre;
+        this.raza = raza;
+
+        this.fuerza = generarValor();
+        this.agilidad = generarValor();
+        this.constitucion = generarValor();
+        this.nivel = 1;
+        this.experiencia = 0;
+        this.puntosDeVida = 50 + generarValor();
+
+
+        vidaInicial = puntosDeVida;
+
+
+
+    }
+
+
+    public Personaje(String nombre) {
+        this.nombre = nombre;
+        this.raza = Raza.HUMANO;
+        this.fuerza = generarValor();
+        this.agilidad = generarValor();
+        this.constitucion = generarValor();
+        this.nivel = 1;
+        this.experiencia = 0;
+        this.puntosDeVida = 50 + generarValor();
+    }
+
+
+    private int generarValor(){
+        return new Random().nextInt(0, 101);
+    }
+
+
     private int validarValor(int valor) {
+        return valor < 1 ? 1 : valor;
+    }
+
+    private double validarValor(double valor) {
         return valor < 1 ? 1 : valor;
     }
 
@@ -31,4 +96,28 @@ public class Personaje {
                 + constitucion + ", nivel=" + nivel + ", experiencia=" + experiencia + ", puntosDeVida=" + puntosDeVida;
     }
 
+    byte sumarExperiencia(int puntos){
+
+        this.experiencia += puntos;
+
+        while (puntos > 1000) {
+            this.nivel++;
+            puntos-=1000;
+        }
+
+        return this.nivel;
+    }
+
+
+    public void subirNivel(){
+        this.nivel++;
+
+        this.fuerza = fuerza + fuerza*0.05;
+        this.agilidad = agilidad + agilidad*0.05;
+        this.constitucion = constitucion + constitucion*0.05;
+    }
+
+    public void curar(){
+
+    }
 }
--- src/ud5/Practicas/Rol/app.java
+++ src/ud5/Practicas/Rol/app.java
@@ -2,7 +2,14 @@ package ud5.Practicas.Rol;
 
 public class app {
     public static void main(String[] args) {
-        Personaje barbudo = new Personaje("Barbudo", 100, 10, 80, -1, -1, 0);
+        Personaje barbudo = new Personaje("Barbudo");
+
+        barbudo.sumarExperiencia(1000);
+
+
+        System.out.println(barbudo.toString());
+
+        barbudo.subirNivel();
 
         System.out.println(barbudo.toString());
     }
