4223ebeab2b50e159bcb59829587e0c894382794
Yago Otero <yagootero2003@gmail.com>
2025-02-27 14:18:46 +0100

    Ejercicio 27 del 02

--- src/ud5/clasesejercicios/EP0821/EP0821.java
+++ src/ud5/clasesejercicios/EP0821/EP0821.java
@@ -2,9 +2,24 @@ package ud5.clasesejercicios.EP0821;
 
 public class EP0821 {
 
-
-    static boolean esNumero(Object ob){
+    static boolean esNumero(Object ob) {
         return ob instanceof Number;
+    }
+
+    static boolean sumar(Object a, Object b) {
+        if (esNumero(a) && esNumero(b)) {
+            Double r = (((Number) a).doubleValue() + ((Number) b).doubleValue());
+
+            if (r - r.longValue() == 0) {
+
+            }
+        } else if (a instanceof String && b instanceof String) {
+            System.out.println("La concatenacion es: " +a + b);
+        } else{
+            System.out.println("No sumables");
+        }
+
+        return true;
     }
 
     public static void main(String[] args) {
@@ -19,9 +34,6 @@ public class EP0821 {
 
         // Double n2 = (double) n;
         // System.out.println(n.getClass().getSimpleName());
-
-        
-        
 
     }
 }
