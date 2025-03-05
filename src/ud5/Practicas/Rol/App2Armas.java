21acbf7366ca0427feb8f58da50d293c67c80be5
Yago Otero <yagootero2003@gmail.com>
2025-03-06 00:28:56 +0100

    Ejercicio 7 progra

--- src/ud5/Practicas/Rol/App2Armas.java
+++ src/ud5/Practicas/Rol/App2Armas.java
@@ -6,7 +6,8 @@ public class App2Armas{
     public static void main(String[] args) {
         PersonajeEx p = new PersonajeEx("Oscar");
 
-        Armadura armadura = new Armadura("Yelmo de plata", 10, 100, 5, TipoDefensa.YELMO);
+        Armadura armadura = new Armadura("Yelmo de plata", 10, 100, 5, TipoDefensa.ESCUDO);
+        Arma arma = new Arma("Espada de hierro", 20, 100, 50);
 
         p.equipar(armadura);
     }
--- src/ud5/Practicas/Rol/PersonajeEx.java
+++ src/ud5/Practicas/Rol/PersonajeEx.java
@@ -2,6 +2,8 @@ package ud5.Practicas.Rol;
 
 import java.util.Arrays;
 
+import ud5.Practicas.Rol.Armadura.TipoDefensa;
+
 // TODO Que las monedas no sean negativas 
 
 public class PersonajeEx extends Personaje {
@@ -9,17 +11,19 @@ public class PersonajeEx extends Personaje {
     Item[] items;
     private double pesoMaximo;
 
-    Item itemManoIzq ;
+    Item itemManoIzq;
     Item itemManoDch;
     Armadura armaduraCabeza;
     Armadura armaduraCuerpo;
+
+    private int manosOcupadas;
 
     public PersonajeEx(String nombre, int fuerza, int agilidad, int constitucion, byte nivel, int experiencia,
             int puntosDeVida, Raza raza, int monedas, Item[] items) {
         super(nombre, fuerza, agilidad, constitucion, nivel, experiencia, puntosDeVida, raza);
         this.monedas = monedas;
         this.items = items;
-        this.pesoMaximo = (constitucion + 50) *2;
+        this.pesoMaximo = (constitucion + 50) * 2;
     }
 
     public PersonajeEx(String nombre, Raza raza, int fuerza, int agilidad, int constitucion, int monedas,
@@ -28,7 +32,7 @@ public class PersonajeEx extends Personaje {
         this.monedas = monedas;
         this.items = items;
 
-        this.pesoMaximo = (constitucion + 50) *2;
+        this.pesoMaximo = (constitucion + 50) * 2;
     }
 
     public PersonajeEx(String nombre, Raza raza, int monedas, Item[] items) {
@@ -36,18 +40,19 @@ public class PersonajeEx extends Personaje {
         this.monedas = monedas;
         this.items = items;
 
-        this.pesoMaximo = (constitucion + 50) *2;
-        
+        this.pesoMaximo = (constitucion + 50) * 2;
+
     }
 
-    // Dejé un constructor como default en el que solo llama al nombre de Personaje y los demas se añade mas tarde
+    // Dejé un constructor como default en el que solo llama al nombre de Personaje
+    // y los demas se añade mas tarde
 
     public PersonajeEx(String nombre, int monedas, Item[] items) {
         super(nombre);
         this.monedas = monedas;
         this.items = items;
 
-        this.pesoMaximo = (constitucion + 50) *2;
+        this.pesoMaximo = (constitucion + 50) * 2;
     }
 
     public PersonajeEx(String nombre) {
@@ -55,32 +60,29 @@ public class PersonajeEx extends Personaje {
         this.monedas = 0;
         this.items = new Item[0];
 
-        this.pesoMaximo = (constitucion + 50) *2;
+        this.pesoMaximo = (constitucion + 50) * 2;
     }
 
-    double pesoActualInventario(){
+    double pesoActualInventario() {
         double peso = 0;
 
-        for (Item itemsLista: items){
+        for (Item itemsLista : items) {
             peso += itemsLista.peso;
         }
 
         return peso;
     }
 
-
-    boolean addToInventario(Item item){
-
-
+    boolean addToInventario(Item item) {
 
         // Peso con el ITEM puesto.
-        
+
         if (item.peso + pesoActualInventario() > pesoMaximo) {
             System.out.println("El item excede el peso que puede transportar el Monstruo");
             return false;
-        } else{
-            items = Arrays.copyOf(	items, items.length+1);
-            items[items.length-1] = item;
+        } else {
+            items = Arrays.copyOf(items, items.length + 1);
+            items[items.length - 1] = item;
 
             System.out.println("Item añadido correctamente");
 
@@ -88,16 +90,17 @@ public class PersonajeEx extends Personaje {
         }
     }
 
-    void mostrarInformacion(){
+    void mostrarInformacion() {
         System.out.println("1. Inventario de personaje");
         System.out.println("==========================");
         System.out.println("\nInventario de " + nombre);
-        
+
         for (int i = 0; i < items.length; i++) {
-            System.out.println(i+". " + items[i].nombre+", peso: " + items[i].peso + ", precio: " + items[i].precioBase + monedas);
+            System.out.println(i + ". " + items[i].nombre + ", peso: " + items[i].peso + ", precio: "
+                    + items[i].precioBase + monedas);
         }
 
-        System.out.println("\nCarga total transportada: " + pesoActualInventario() + "/" + pesoMaximo  + " kilos");
+        System.out.println("\nCarga total transportada: " + pesoActualInventario() + "/" + pesoMaximo + " kilos");
     }
 
     @Override
@@ -105,22 +108,67 @@ public class PersonajeEx extends Personaje {
         return "1. Inventario de personaje";
     }
 
-    
-    boolean equipar(Item itemManoIzq, Item itemManoDch, Armadura armaduraCabeza, Armadura armaduraCuerpo ){
+    boolean equipar(Item item) {
 
+        if (item instanceof Armadura) {
+            Armadura armadura = (Armadura) item;
 
-       this.itemManoDch = itemManoDch;
+            if (armadura.tipoDefensa == TipoDefensa.ESCUDO) {
+                if (itemManoDch == null) {
+                    itemManoDch = armadura;
+                    return true;
+                } else if (itemManoIzq == null) {
+                    itemManoIzq = armadura;
+                    return true;
+                } else {
+                    return false;
+                }
+            } else if (armadura.tipoDefensa == TipoDefensa.YELMO) {
+                if (armaduraCabeza == null) {
+                    armaduraCabeza = armadura;
+                    return true;
+                } else {
+                    return false;
+                }
+                // Es armadura pero no escud
+            } else {
+                // Es armadura por narices
+                if (armaduraCuerpo == null) {
+                    armaduraCuerpo = armadura;
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        } else if (item instanceof Arma) {
+            Arma arma = (Arma) item;
+            if (arma.dosManos) {
+                if (itemManoDch == null && itemManoIzq == null) {
+                    itemManoDch = arma;
+                    itemManoIzq = arma;
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                // El arma es de una mano
+                if (itemManoDch == null) {
+                    itemManoDch = arma;
+                    return true;
+                } else if (itemManoIzq == null) {
+                    itemManoIzq = arma;
+                    return true;
+                } else {
+                    return false;
+                }
 
-       if (itemManoIzq) {
-        
-       }
+                
+            }
+        } else {
 
-
-
-
-        return false;
+            // No es ni arma ni armadura =?'='
+            return false;
+        }
     }
-    
-    
 
 }
