0659b50f16f0f56014bc1fd7d2f895aa8b6284f9
Yago Otero <yagootero2003@gmail.com>
2025-03-27 22:58:09 +0100

    ejericcios bases catastro 63 -> 72

---
+++ src/AceptaElReto/ArraysMultidimensionales/CasillaSnake.java
@@ -0,0 +1,12 @@
+package AceptaElReto.ArraysMultidimensionales;
+
+public class CasillaSnake {
+
+    int fila, columna , repeticiones;
+
+    public CasillaSnake(int fila, int columna, int repeticiones) {
+        this.fila = fila;
+        this.columna = columna;
+        this.repeticiones = repeticiones;
+    }
+}
--- src/AceptaElReto/ArraysMultidimensionales/Snake.java
+++ src/AceptaElReto/ArraysMultidimensionales/Snake.java
@@ -1,5 +1,6 @@
 package AceptaElReto.ArraysMultidimensionales;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -8,6 +9,7 @@ public class Snake {
         char[][] mapa = new char[5][5];
         Random rdm = new Random();
 
+        mapa = rellenarEspacios(mapa);
         mapa[mapa.length / 2][mapa[0].length / 2] = '*';
         mapa[rdm.nextInt(5)][rdm.nextInt(5)] = '^';
 
@@ -19,13 +21,16 @@ public class Snake {
 
     static boolean jugar(char[][] mapa) {
         Scanner sc = new Scanner(System.in);
+        Random rdm = new Random();
         boolean muerto = false;
         int longitudSnake = 1;
+        CasillaSnake[] repeticiones = new CasillaSnake[0];
 
         int filaSnake = buscarSnake(mapa).charAt(0);
         int columnaSnake = buscarSnake(mapa).charAt(2);
 
-        
+        int filaSnakeAnterior = filaSnake;
+        int columnaAnteriorSnake = columnaSnake;
 
         do {
             mostrarMatriz(mapa);
@@ -40,8 +45,13 @@ public class Snake {
                         return false;
                     } else {
                         filaSnake -= 1;
+                        columnaAnteriorSnake = columnaSnake;
+                        filaSnake = filaSnakeAnterior;
                         if (comprobarComida(mapa, filaSnake, columnaSnake)) {
                             longitudSnake++;
+                            repeticiones = Arrays.copyOf(repeticiones, repeticiones.length + 1);
+                            repeticiones[repeticiones.length - 1] = new CasillaSnake(filaSnake, columnaSnake,
+                                    longitudSnake);
                         }
                     }
                     break;
@@ -49,38 +59,88 @@ public class Snake {
                     if (filaSnake == mapa.length - 1) {
                         return false;
                     } else {
-                        filaSnake += 1;
+                        columnaAnteriorSnake = columnaSnake;
+                        filaSnake = filaSnakeAnterior;
                         if (comprobarComida(mapa, filaSnake, columnaSnake)) {
                             longitudSnake++;
+                            repeticiones = Arrays.copyOf(repeticiones, repeticiones.length + 1);
+                            repeticiones[repeticiones.length - 1] = new CasillaSnake(filaSnake, columnaSnake,
+                            longitudSnake);
                         }
+                        filaSnake += 1;
                     }
+                    break;
                 case 'A':
                     if (columnaSnake == 0) {
                         return false;
                     } else {
                         columnaSnake -= 1;
+                        columnaAnteriorSnake = columnaSnake;
+                        filaSnake = filaSnakeAnterior;
                         if (comprobarComida(mapa, filaSnake, columnaSnake)) {
                             longitudSnake++;
+                            repeticiones = Arrays.copyOf(repeticiones, repeticiones.length + 1);
+                            repeticiones[repeticiones.length - 1] = new CasillaSnake(filaSnake, columnaSnake,
+                                    longitudSnake);
                         }
+
                     }
+                    break;
                 case 'D':
                     if (columnaSnake == mapa[columnaSnake].length - 1) {
                         return false;
                     } else {
+
                         columnaSnake += 1;
+                        columnaAnteriorSnake = columnaSnake;
+                        filaSnake = filaSnakeAnterior;
                         if (comprobarComida(mapa, filaSnake, columnaSnake)) {
                             longitudSnake++;
+                            repeticiones = Arrays.copyOf(repeticiones, repeticiones.length + 1);
+                            repeticiones[repeticiones.length - 1] = new CasillaSnake(filaSnakeAnterior,
+                                    columnaAnteriorSnake,
+                                    longitudSnake);
+
+                            mapa[rdm.nextInt(5)][rdm.nextInt(5)] = '^';
+
                         }
+
                     }
                 default:
                     break;
             }
+
+            mapa = rellenarRepeticiones(mapa, repeticiones);
+
             mapa[filaSnake][columnaSnake] = '*';
-            
+            restarRepeticiones(mapa, repeticiones);
 
         } while (!muerto);
 
         return muerto;
+    }
+
+    static char[][] restarRepeticiones(char[][] mapa, CasillaSnake[] repeticiones) {
+        for (int i = 0; i < repeticiones.length; i++) {
+            repeticiones[i].repeticiones--;
+        }
+
+        return mapa;
+    }
+
+    static char[][] rellenarRepeticiones(char[][] mapa, CasillaSnake[] repeticiones) {
+        for (int i = 0; i < repeticiones.length; i++) {
+            if (repeticiones[i].repeticiones > 0) {
+                CasillaSnake csilla = repeticiones[i];
+                mapa[csilla.fila][csilla.columna] = '*';
+            } else {
+                CasillaSnake csilla = repeticiones[i];
+                mapa[csilla.fila][csilla.columna] = ' ';
+
+            }
+        }
+
+        return mapa;
     }
 
     static boolean comprobarComida(char[][] mapa, int fila, int columna) {
@@ -89,6 +149,16 @@ public class Snake {
         } else {
             return false;
         }
+    }
+
+    static char[][] rellenarEspacios(char[][] mapa) {
+        for (int i = 0; i < mapa.length; i++) {
+            for (int j = 0; j < mapa[i].length; j++) {
+                mapa[i][j] = ' ';
+            }
+        }
+
+        return mapa;
     }
 
     static String buscarSnake(char[][] mapa) {
