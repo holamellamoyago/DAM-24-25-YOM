3fce50ebde9a063909e169821a07420efcfe86da
Yago Otero <yagootero2003@gmail.com>
2025-04-01 23:07:34 +0200

    Ejercicios 01-04

--- src/ud6/apuntescolecciones/E1208.java
+++ src/ud6/apuntescolecciones/E1208.java
@@ -1,21 +1,32 @@
 package ud6.apuntescolecciones;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.Random;
 
 public class E1208 {
-    public static void main(String[] args) {
+    public static <T> void main(String[] args) {
         Collection<Integer> coleccion = new ArrayList<>();
 
-        for (int i = 0; i <= 20; i++) {
+        for (int i = 0; i < 20; i++) {
             int rdm = new Random().nextInt(100);
             coleccion.add(rdm);
         }
 
         // Ordeno
+        Integer[] a = coleccion.toArray(new Integer[0]);
+        Arrays.sort(a);
+        System.out.println("DE MENOR A MAYOR");
+        System.out.println(Arrays.toString(a));
+
+        System.out.println("DE MAYOR A MENOR");
+        Arrays.sort(a, Comparator.reverseOrder());
+        coleccion = new ArrayList<>(Arrays.asList(a));
+        System.out.println(coleccion.toString());
         
-        Integer[] array  = new Integer[0];
+        
 
     }
 }
