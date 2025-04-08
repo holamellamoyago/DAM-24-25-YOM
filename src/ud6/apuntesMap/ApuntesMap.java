62ebd3724628a9157d4faac8476736c0d0823269
Yago Otero <yagootero2003@gmail.com>
2025-04-08 14:25:58 +0200

    Ejercicios 08/04

---
+++ src/ud6/apuntesMap/ApuntesMap.java
@@ -0,0 +1,37 @@
+package ud6.apuntesMap;
+
+import java.util.HashMap;
+import java.util.TreeMap;
+import java.util.Map;
+import java.util.Set;
+
+public class ApuntesMap {
+    public static void main(String[] args) {
+        Map<String, Integer> m = new TreeMap<>();
+
+        m.put("Yago", 180);
+        m.put("Eva", 160);
+        m.put("Yago", 190);
+        m.remove("Eva");
+
+        System.out.println(m.toString());
+
+        m.clear();
+        System.out.println(m);
+
+        m.put("Eva", 160);
+        System.out.println(m.get("Eva"));
+
+        System.out.println(m.containsKey("Yago"));
+        System.out.println(m.containsKey("Eva"));
+
+        System.out.println(m.containsValue(160));
+
+        // Vistas Collectios de los mapas
+        System.out.println("\n  Vistas Collectios de los mapas");
+        m.put("Pepe", 170);
+        Set<String> s = m.keySet();
+        System.out.println(s);
+
+    }
+}
